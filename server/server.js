61f663c57467c8503cb4cdc233cc4ddbffe717a8
abhinav04-b <76025844+abhinav04-b@users.noreply.github.com>
2025-12-29 15:23:00 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -52,8 +52,9 @@ app.use(express.static(path.join(__dirname, '../dist')));
 
 // 2. The "catch-all" route: If the user goes to /about or /admin, 
 // send them index.html so React Router can take over.
-app.get('/*', (req, res) => {
+app.get('/:any*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
